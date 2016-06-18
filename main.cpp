f2b47997613ef9969151c399c2bc87ef92399f76
Jan Kroeze <thejcwk@gmail.com>
2016-06-18 16:24:17 +0200

    Create a window.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,36 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+const char window_class_name[] = "myWindowClass";
+const char window_title[] = "My Window";
+const int window_width = 640;
+const int window_height = 480;
+
+int error(const char* msg) {
+	LOG(ERROR) << msg;
+	MessageBox(NULL, msg, "Error", MB_ICONEXCLAMATION | MB_OK);
+	return 1;
+}
+
+LRESULT CALLBACK
+WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI
+WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 				   LPSTR lpCmdLine, int nCmdShow) {
+	/* Setup logging. */
 	int argc = 0;
 	LPCWSTR cmd_line = GetCommandLineW();
 	LPWSTR* argvw = CommandLineToArgvW(cmd_line, &argc);
@@ -19,6 +47,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	}
 	START_EASYLOGGINGPP(argc, (char**)argv);
 	LOG(TRACE) << "Logging initialized.";
-	MessageBox(NULL, "Hello, world!", "Note", MB_OK);
-	return 0;
+
+	/* Register window class. */
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = window_class_name;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	if (!RegisterClassEx(&wc)) return error("Could not register window class.");
+
+	/* Create window. */
+	HWND hwnd;
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE, window_class_name, window_title, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, window_width, window_height,
+		NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) return error("Could not create window.");
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	/* Enter event loop. */
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
 }
